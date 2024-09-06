206b492dbf3014e85262c318f7ca2128a58c07b3
HTML5Cpp <68476621+HTML5Cpp@users.noreply.github.com>
2024-09-06 09:22:59 -0600

    Add files via upload

--- JS/cv.js
+++ JS/cv.js
@@ -1,6 +1,6 @@
 document.getElementById("btnCV").addEventListener("click", function() {
     const link = document.createElement("a");
-    link.href = "../MEDIA/CV_ODSZ_Espanol.pdf";
+    link.href = "./MEDIA/CV_ODSZ_Espanol.pdf";
     link.download = "CV_ODSZ_Espanol.pdf";
     link.click();
 });
